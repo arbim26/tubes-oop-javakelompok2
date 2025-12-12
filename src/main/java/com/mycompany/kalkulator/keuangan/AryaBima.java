cdbc9de0dd5a2c010cbc9117d2e895d2c984287a
arbim6 <arbiim26@gmail.com>
2025-12-12 11:22:50 +0700

    Add file : AryaBima.java

--- src/main/java/com/mycompany/kalkulator/keuangan/AryaBima.java
+++ src/main/java/com/mycompany/kalkulator/keuangan/AryaBima.java
@@ -2,6 +2,6 @@ package com.mycompany.kalkulator.keuangan;
 
 public class AryaBima {
     public static void main(String[] args) {
-        System.out.println("Hello Arya");
+        System.out.println("Nama :Arya Bima Setyono NIM : 1324054");
     }
 }
